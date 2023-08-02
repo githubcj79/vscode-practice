a6a4461529c74aa7fb257002536fad1430f88a6e
carlos <carlos.jimenez@gmail.com>
2023-08-02 16:34:03 -0400

    docstring added

--- sum.py
+++ sum.py
@@ -1,5 +1,8 @@
-# function that takes a list of numbers as input and returns the sum of the numbers.
 def add(numbers):
+    '''
+    This function takes a list of numbers as input 
+    and returns the sum of the numbers.
+    '''
     total = 0
     for i in numbers:
         total += i
